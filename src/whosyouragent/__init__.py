6047bf56e807bf9fe27a4b40cbf40a507a792c90
matt manes <mattmanes@pm.me>
2023-01-21 16:12:32 -0600

    Removed tests from dist/.

--- src/whosyouragent/__init__.py
+++ src/whosyouragent/__init__.py
@@ -1,3 +1,4 @@
+import json
 import os
 import time
 from pathlib import Path
@@ -7,8 +8,8 @@ from .whosyouragent import VersionUpdater, get_agent
 browsers_path = Path(__file__).parent / "browserVersions.json"
 if (
     not browsers_path.exists()
-    or time.time() - os.stat(str(browsers_path)).st_mtime > 604800
-):  # 1 week
+    or time.time() - os.stat(str(browsers_path)).st_mtime > 604800  # 1 week
+):
     print("Updating whosyouragent browser versions...")
     updater = VersionUpdater()
     updater.update_all()
