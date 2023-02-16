80121d47285efa4e023e8eb9b0bfb344e2c99765
matt manes <mattmanes@pm.me>
2023-02-16 14:59:15 -0600

    fix: update chrome version updater

--- src/whosyouragent/whosyouragent.py
+++ src/whosyouragent/whosyouragent.py
@@ -29,7 +29,7 @@ class VersionUpdater:
             url = "https://en.wikipedia.org/wiki/Google_Chrome"
             soup = BeautifulSoup(requests.get(url).text, "html.parser")
             info_boxes = soup.find_all("td", class_="infobox-data")
-            version = info_boxes[8].text[: info_boxes[8].text.find("/")]
+            version = info_boxes[8].text[: info_boxes[8].text.find("[")]
             self.chrome = version
         except Exception as e:
             print(e)
